21f2841e3d6e9b898e11bdba08445ab3cb1fd29e
Rory MacDonald <rory@madebymade.co.uk>
2017-06-24 11:25:49 +0100

    add paginator

---
+++ app/lib/sar/spareroom/search_result_page.rb
@@ -0,0 +1,48 @@
+module SAR
+  module Spareroom
+    class SearchResultPage
+      def initialize(response)
+        @response = response
+        @response.search(xpath_price_within_comments).each do |element|
+          price = parse_price(element)
+          unless price.empty?
+            results << price
+          end
+        end
+      end
+
+      def next_page
+        unless is_last?
+          @response.at(xpath_next_page_link)
+        end
+      end
+
+      def results
+        @results ||= []
+      end
+
+      def is_last?
+        if @response.at(xpath_next_page_link)
+          return false
+        else
+          return true
+        end
+      end
+
+      private
+
+      def parse_price(element)
+        element.to_s.gsub(/\D/, '')
+      end
+
+      def xpath_next_page_link
+        '//*[@id="maincontent"]/div[2]/ul[2]/li/strong/a'
+      end
+
+      def xpath_price_within_comments
+        '//*[@id="maincontent"]/ul/li/article/header[1]/a/strong/comment()'
+      end
+
+    end
+  end
+end
