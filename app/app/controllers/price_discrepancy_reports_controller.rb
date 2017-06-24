5a43a83e24e9a356d5573ab4b4515aedc3c66938
Emile Swarts <emile@madetech.com>
2017-06-24 11:43:47 +0100

    Form post endpoint (#9)
    * Scaffold
    
    * Add form
    
    * Fixup
    
    * Fixup

---
+++ app/app/controllers/price_discrepancy_reports_controller.rb
@@ -0,0 +1,7 @@
+class PriceDiscrepancyReportsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :price_discrepancy_reports, only: [:index, :create]
+
   root to: 'static#index'
 end
