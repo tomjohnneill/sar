4d24ca0176a6343d53c2e18a9347238cafd1d5db
Chris Blackburn <christopher.blackburn@gmail.com>
2017-06-24 13:52:25 +0100

    Ensure libs are loaded in production

--- app/config/application.rb
+++ app/config/application.rb
@@ -10,6 +10,12 @@ module App
   class Application < Rails::Application
     config.load_defaults 5.1
 
+    config.to_prepare do
+      Dir.glob(File.join(File.dirname(__FILE__), '../lib/sar/**/*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
     config.autoload_paths << Rails.root.join('lib')
   end
 end
